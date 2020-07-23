43745ff9c23fbbfa5c42dd0a6dfe250ae5be2d49
karanawate <karan.a@ideatore.in>
2020-07-23 12:29:31 +0530

    One user id store in to another table in laravel

--- app/mobile.php
+++ app/mobile.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class mobile extends Model
 {
+    protected $table = 'mobiles';
+
+    protected $fillable =[
+        'number',
+        'user_id'
+    ];
+    protected $dates =[
+        'created_at',
+        'updated_at'
+    ];
     public function user()
     {
         return $this->belongsTo(User::class);
