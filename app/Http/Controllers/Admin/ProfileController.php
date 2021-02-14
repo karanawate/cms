d083a00aa75621bc1d0536eb9d30208fc1ba03e3
karanawate <karanawate8@gmail.com>
2021-02-15 01:25:07 +0530

    select query sysyntax

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 use Auth;
+use DB;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -10,7 +11,8 @@ class ProfileController extends Controller
     public function index(Request $request)
     {
         $current_user = Auth::user()->id;
-        dd($current_user);
+        $users = \DB::table('users')->where('id', $current_user)->get();
+        dd($users);
         return view('admin/profile');
     }
 
